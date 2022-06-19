8ee3d6cee2f1af4547fde3446e3bc7fa8cfaa48f
Jorge <jorch.rl@gmail.com>
2022-06-19 08:47:04 -0500

    Add an useStyles() custom hook (WIP)

---
+++ hooks/useStyles.ts
@@ -0,0 +1,17 @@
+import { tw } from "$twind";
+
+// This hook is intended to let you implement global styles
+// as well as a function for overriding and adding your own,
+// per component.
+
+export function useStyles() {
+  const styles = {
+    headerText: tw`text-2xl text-black font-serif font-bold`,
+    bodyText: tw`font-sans text-blue-500 font-light text-base`,
+  };
+
+  const setStyles = () => {
+  };
+
+  return { styles, setStyles };
+}
