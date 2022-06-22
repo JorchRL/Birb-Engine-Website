0ada7d47aa52fe7fc3924b511c0a8117a1dfd4aa
Jorge <jorch.rl@gmail.com>
2022-06-22 18:48:31 -0500

    Add boilerplate Info.tsx component

---
+++ components/Info.tsx
@@ -0,0 +1,13 @@
+/**@jsx h */
+/**@jsxFrag Fragment */
+
+import { Fragment, h } from "$fresh/runtime.ts";
+import { tw } from "$twind";
+
+export default function Info() {
+  return (
+    <>
+      <div>Some info about the app. How it works and so...</div>
+    </>
+  );
+}
